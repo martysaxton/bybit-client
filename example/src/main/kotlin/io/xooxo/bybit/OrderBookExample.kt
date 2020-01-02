4265c3bdefaf295a46a81141d3687ea5c7ac3da2
Marty Saxton <marty@xooxo.io>
2020-01-01 21:57:09 -0600

    added trades to orderbook example

--- example/src/main/kotlin/io/xooxo/bybit/OrderBookExample.kt
+++ example/src/main/kotlin/io/xooxo/bybit/OrderBookExample.kt
@@ -7,7 +7,9 @@ fun main(args: Array<String>) {
     val client = ByBitClientImpl(ByBitClient.Type.LIVE)
     val book = OrderBook()
     client.setCloseListener { println("websocket closed") }
-//    client.setTradeListener { println("trade: $it") }
+    client.setTradeListener {
+        println("trade: $it")
+    }
 //    client.setInstrumentInfoListeners(
 //            { println("instrument info snapshot:  $it") },
 //            { println("instrument info delta: $it") }
@@ -24,7 +26,7 @@ fun main(args: Array<String>) {
     )
     client.setConnectListener {
         println("websocket connected")
-//        client.subscribeToTrades()
+        client.subscribeToTrades()
 //        client.subscribeToInstrumentInfo("BTCUSD")
         client.subscribeToOrderBook("BTCUSD")
     }
